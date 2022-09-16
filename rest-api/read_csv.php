0f02b1799b7a61f57bb8bb4010b459fdabd422d8
Diego Lopez <ndzerglink@live.jp>
2022-09-16 10:40:46 +0900

    ReadCSV, updateDB, and redir HOME works

--- rest-api/read_csv.php
+++ rest-api/read_csv.php
@@ -9,14 +9,14 @@
     <body>
         <h2>Updating DB</h2>
 <?php
-ini_set('display_errors',1);
+
 date_default_timezone_set("Asia/Tokyo");
 $heute = date("md");
 $data_dir = "../data/";
 $data_file = "tenki_hour_" . $heute . ".csv";
 
 echo "<h3> Welcome: ".$_POST["my_id"]."</h3";
-echo "<p>Reading: ".$data_dir.$data_file)."</p>";
+echo "<p>Reading: ".$data_dir.$data_file."</p>";
 
 // and verify data is not yet present on DB
 $heute = date ("Y-m-d");
@@ -24,17 +24,16 @@ $dbTable = "weather_data";
 $existQry = "SELECT date FROM $dbTable WHERE EXISTS 
 (SELECT date FROM $dbTable WHERE date=$heute)";
 
-require_once "inc/config.php";
-$dbHandle = mysqli_connect(DB_HOST,DB_USER,$_POST["my_pass"],DB_NAME);
-if(mysqli_connect_errno()){
-    die("Couldnt connect to DB,".mysqli_connect_error());
-}
-
 $auxQuery = "";
 $auxStr = "";
 $upQuery = "INSERT INTO " . $dbTable . " VALUES(";
 
 if(file_exists($data_dir.$data_file)){
+    require_once "inc/config.php";
+    $dbHandle = mysqli_connect(DB_HOST,DB_USER,$_POST["my_pass"],DB_NAME);
+    if(mysqli_connect_errno()){
+        die("Couldnt connect to DB,".mysqli_connect_error());
+    }
     $fileHandle = fopen($data_dir.$data_file,"r");
     while(($row = fgetcsv($fileHandle,0,",")) !== FALSE){
         if (count($row) == 1 && empty($row[0])){
@@ -65,12 +64,21 @@ if(file_exists($data_dir.$data_file)){
             else{echo "done</p>";}
         }
     }
-    mysqli_close($dbHandle);
 }else{
     echo "<p>Couldnt find such file :(</p>";
 }
 
 fclose($fileHandle);
+mysqli_close($dbHandle);
+
+// Redirect to home
+echo "<h2>Redirecting to home</h2>";
+sleep(15);
+ob_start();
+header('Location: //webapp.physics');
+ob_end_flush();
+exit();
+
 // printf($existQry);
 ?>
 </body>

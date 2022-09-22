332907ec0a771092ad76e3f93a965e1e3bd5dcd7
Diego Lopez <ndzerglink@live.jp>
2022-09-22 10:37:57 +0900

    Added a func to load either form

--- static/fill_date.js
+++ static/fill_date.js
@@ -12,3 +12,21 @@ for(let idx=1; idx<=31;idx++){
     el.innerText = strIdx;
     selDiv.appendChild(el);
 }
+
+function openForm(ev, thisForm){
+    var idx,tabcontent,tablink;
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (idx=0; idx <tabcontent.length; idx++){
+        tabcontent[idx].style.display = "none";
+    }
+    tablink = document.getElementsByClassName("tabLink");
+    for(idx = 0; idx< tablink.length; idx++){
+        tablink[idx].className = tablink[idx].className.replace(" active","");
+    }
+
+    document.getElementById(thisForm).style.display = "block";
+    ev.currentTarget.className += " active";
+
+}
+
+document.getElementById("default").click();

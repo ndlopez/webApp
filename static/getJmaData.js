1f9eaede14e4ae6c503c1f4a256e94163ac752a5
Diego Lopez <ndzerglink@live.jp>
2022-09-26 17:28:10 +0900

    Validated max/min when time -gt 12

--- static/getJmaData.js
+++ static/getJmaData.js
@@ -33,18 +33,26 @@ async function disp_info(){
     iconElm.setAttribute("class","col3");
 
     var texty = "<br/><p>"+gotData.weather[1] +"</p>";
-    texty += "<span>Min "+ gotData.temp[1][2]+"&#8451; | Max "+ gotData.temp[1][3]+"&#8451;</span>";
+    var myMin = gotData.temp[1][2];
+    var myMax = gotData.temp[1][3];
+    if(myMax === undefined){
+        myMax = gotData.temp[1][1];
+        myMin = gotData.temp[1][0];
+    }
+    texty += "<span>Min "+ myMin +"&#8451; | Max "+ myMax+"&#8451;</span>";
     iconElm.innerHTML = "<img src='"+ico_url+gotData.icon[1]+".svg'/>"+texty;
     const tempElm = document.createElement("div");//tomorrow temp
     tempElm.setAttribute("class","col3");
     
     texty = "";
-    let jdx = gotData.rain[0].length-1
+    let jdx = gotData.rain[0].length-1;
+    let kdx = 0;
     var textW = "<p>降水確率[%]</p><div class='row'>";
     for(let idx = jdx-3;idx < jdx+1;idx++){
         const get_date = getDateHour(gotData.rain[0][idx]);
-        texty += "<p class='col4'>"+get_date.heure+" - "+hh[idx-2]+"<br/>"+gotData.rain[1][idx]+"%</p>";
+        texty += "<p class='col4'>"+get_date.heure+" - "+hh[kdx]+"<br/>"+gotData.rain[1][idx]+"%</p>";
         //console.log(gotData.rain[0].length,texty);
+	kdx++;
     }
     texty += "</div>";
     tempElm.innerHTML = textW + texty;

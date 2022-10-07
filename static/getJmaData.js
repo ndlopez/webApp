e5e62704388922eefb133d442cc713e89f4a171b
Diego Lopez <ndzerglink@live.jp>
2022-10-07 12:18:27 +0900

    Styled a lil week dates

--- static/getJmaData.js
+++ static/getJmaData.js
@@ -9,7 +9,7 @@ const sun_time = ["https://dayspedia.com/api/widget/city/11369/?lang=en",
 const hh = [5,11,17,23];
 const theseMonths = ["January","February","March","April","May","June","July",
 "August","September","October","November","December"];
-const theseDays = ["Sun, ","Mon, ","Tue, ","Wed, ","Thu, ","Fri, ","Sat, "];
+const theseDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 disp_info();
 
@@ -55,15 +55,19 @@ async function disp_info(){
     for(let idx=0;idx<gotData.forecast[0].length;idx++){
         const groupDiv = document.createElement("div");
         groupDiv.setAttribute("class","row");
-        texty = "<div class='col3'><img src='"+ico_url+ gotData.forecast[1][idx]+".svg'/></div>";
+
         var aux = getDateHour(gotData.forecast[0][idx]);
         var tempMin = gotData.forecast[2][idx], tempMax = gotData.forecast[3][idx];
-        texty += "<div class='col3' style='text-align:left;'><p>"+theseDays[aux.day] + theseMonths[aux.monty-1]+" "+aux.tag+"</p></div>";
+        texty = "<div class='col3' style='margin:0;border-radius:inherit;'><p>" + theseDays[aux.day] + 
+        "</p><h2>"+aux.tag+"</h2><p>"+theseMonths[aux.monty-1]+"</p></div>";
+
+        texty += "<div class='col3'><img src='"+ico_url+ gotData.forecast[1][idx]+".svg'/></div>";
+
         if(idx==0){
             tempMin = myMin;
             tempMax = myMax;
         }
-        texty += "<div class='col3'><p>"+tempMin+"&#8451; | "+tempMax+"&#8451;</p></div>";
+        texty += "<div class='col3'><h4>"+tempMin+"&#8451; | "+tempMax+"&#8451;</h4></div>";
         groupDiv.innerHTML = texty;
         colDiv.appendChild(groupDiv);
     }
